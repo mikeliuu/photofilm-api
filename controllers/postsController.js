ddb3f360c43bbd7f01e16b9a0206039cffe0313c
Mike Liu <mliu8m@gmail.com>
2020-02-28 19:26:44 +0800

    updated ig posts fetching

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -4,21 +4,16 @@ const fetchPosts = async (req, res) => {
   console.log('1 here');
   const { tag } = req.params;
 
-    const posts = await ig.scrapeTag(tag);
-    console.log('get  posts', posts);
-  
-    const NO_OF_POSTS = 30;
-  
-    // const sortByLike = data && data.medias.length > 0 && (data.medias.sort((a,b) => (a.like_count < b.like_count ? 1 : -1)).slice(0,NO_OF_POSTS));
-  
-    console.log('get "/api/posts" success');
-    console.log('get  tag', tag);
-    // console.log('get  sortByLike', sortByLike);
-  console.log('2 mid');
+  const posts = await ig.scrapeTag(tag);
+  console.log('get  posts', posts);
 
-  console.log('3 end');
+  const NO_OF_POSTS = 30;
 
-  return res.status(200).json(tag);
+  const sortByLike = posts && posts.medias.length > 0 && (posts.medias.sort((a,b) => (a.like_count < b.like_count ? 1 : -1)).slice(0,NO_OF_POSTS));
+
+  console.log('get "/api/posts" success');
+
+  return res.status(200).json(sortByLike);
 };
 
 module.exports = {
