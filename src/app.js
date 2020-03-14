7e72cbca5f5bbe95e43e7060d2e210082516ec2a
Mike Liu <mliu8m@gmail.com>
2020-03-14 23:48:46 +0800

    added .env path

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 require("regenerator-runtime/runtime");
 const express = require('express');
 const bodyParser = require('body-parser');
